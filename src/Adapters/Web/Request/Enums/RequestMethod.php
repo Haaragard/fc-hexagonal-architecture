a8315b072d5ae02ecde5e91b7aedb503e6418d00
haaragard <haaragard@gmail.com>
2024-09-23 22:58:30 -0300

    feat: add RequestMethod enum

---
+++ src/Adapters/Web/Request/Enums/RequestMethod.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Adapters\Web\Request\Enums;
+
+enum RequestMethod: string
+{
+    case GET = 'GET';
+    case POST = 'POST';
+    case PUT = 'PUT';
+    case DELETE = 'DELETE';
+    case PATCH = 'PATCH';
+    case OPTIONS = 'OPTIONS';
+}
