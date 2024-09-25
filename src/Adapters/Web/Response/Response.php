985fbdc0ed0344b8615fdfc32cbd33012414e8fc
haaragard <haaragard@gmail.com>
2024-09-25 09:05:29 -0300

    chore: break code to test ci

--- src/Adapters/Web/Response/Response.php
+++ src/Adapters/Web/Response/Response.php
@@ -4,15 +4,4 @@ namespace App\Adapters\Web\Response;
 
 class Response
 {
-    /**
-     * @param array<string, mixed> $data
-     */
-    public function json(array $data, int $statusCode = 200): void
-    {
-        header('Content-Type: application/json');
-
-        http_response_code($statusCode);
-
-        echo json_encode($data) . PHP_EOL;
-    }
 }
