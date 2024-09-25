4f47bf0c9646e7138eb8a95134581cc31f3f0cb6
haaragard <haaragard@gmail.com>
2024-09-25 08:12:35 -0300

    tests: add HttpServer.php test incomplete

---
+++ tests/Unit/Adapters/Web/Server/HttpServerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\Unit\Adapters\Web\Server;
+
+use PHPUnit\Framework\TestCase;
+
+class HttpServerTest extends TestCase
+{
+    public function testRun(): void
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+}
